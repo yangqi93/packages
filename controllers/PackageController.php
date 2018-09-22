89b0f0b1e602f4533f8f59b0e83569e7dc60aa09
yangqi <nutshell.qi@gmail.com>
2018-09-22 13:28:03 +0800

    入库即签收

--- controllers/PackageController.php
+++ controllers/PackageController.php
@@ -82,7 +82,7 @@ class PackageController extends Controller
         $model->received_at = time();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            Yii::$app->session->setFlash('success', $model->sn. ' 入库成功，请继续录入,变更快递公司，请重新选择');
+            Yii::$app->session->setFlash('success', $model->sn. ' 签收成功，请继续录入,变更快递公司，请重新选择');
             return $this->redirect(['create', 'company' => $model->company]);
         }
 
